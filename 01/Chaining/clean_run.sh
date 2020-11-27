9a65a5dcebb1030a7c8754bf135a9c38ca4930ac
Mohamed Mokhtar <m.mokhtar@outlook.it>
2020-11-27 12:59:56 +0200

    Added labs of network

--- 01/Chaining/clean_run.sh
+++
@@ -1,4 +0,0 @@
-# run and clean script
-rm chaining output test\ case\ 1 test\ case\ 2
-g++ *.cpp -o output
-./output
--- 01/Chaining/rc.sh
+++
@@ -1,4 +0,0 @@
-# run and clean script
-rm chaining output overflow test\ case\ 1 test\ case\ 2
-g++ *.cpp -o output
-./output
